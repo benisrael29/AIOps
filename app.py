30541c831e0a5d5b879408a1fc6e9a9c998bc6ec
benisrael29 <israelben009@gmail.com>
2022-01-24 13:57:31 +1000

    xcvxcv

--- app.py
+++ app.py
@@ -10,14 +10,24 @@ def getHello():
         return(str(user))        
     else:
         return app.send_static_file('main.html')
+
 @app.route('/main')
 def getMain():
     return render_template("./main.html")
 
 @app.route('/about')
-def getAbout():
-    return ('welcome to about')
+def getAbout(): 
+    return ('Artificial Intelligence for IT Operations (AIOps) is a term coined by Gartner in 2016 as an industry category for machine learning analytics technology that enhances IT operations analytics.')
 
 @app.route("/hello")
 def hello():
-    return "Hello, AIOps Team!"
+    return ("Hello, AIOps Team!")
+
+@app.route("/test")
+def test():
+    return ("Testing is fun!!")
+
+#token
+@app.route("/loaderio-cf29df6bc3ec7151c1e535b12a47a968.txt")
+def loader():
+    return ('loaderio-cf29df6bc3ec7151c1e535b12a47a968.txt')
